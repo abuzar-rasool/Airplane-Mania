d69cbbf9e3388722e520fdf5a42ac6c73f298f6d
Jawwad <mj05516@st.habib.edu.pk>
2020-11-29 23:53:36 +0500

    Bird Correction

--- bird.hpp
+++ bird.hpp
@@ -88,10 +88,10 @@ public:
     Bird2(SDL_Texture *texture) : Bird(texture, 4)
     {
         SDL_Rect src[4];
-        src[0] = {41, 157, 87, 85};
-        src[1] = {157, 366, 87, 85};
-        src[2] = {41, 481, 87, 85};
-        src[3] = {157, 481, 87, 85};
+        src[0] = {41, 350, 88, 57};
+        src[1] = {157, 366, 88, 41};
+        src[2] = {41, 481, 87, 64};
+        src[3] = {155, 481, 90, 42};
         Bird::setBirdSource(src);
     };
 };
@@ -101,7 +101,7 @@ class Bird3 : public Bird
 public:
     Bird3(SDL_Texture *texture) : Bird(texture, 5)
     {
-        SDL_Rect src[4];
+        SDL_Rect src[4] ;
         src[0] = {300, 133, 87, 85};
         src[1] = {416, 45, 87, 85};
         src[2] = {300, 172, 87, 85};
